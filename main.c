7e8bfbc71db5c6d88717913884054653ca0cc403
SeHyun11103 <dotori506@gmail.com>
2023-10-12 15:50:18 +0900

    2

--- main.c
+++ main.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_star()
-{
-     int i;
-     for(i=0;i<10;i++)
-       printf("*");
-       }
-
-
-int main(int argc, char *argv[])
-
-{
-    print_star();
-    print_star();
-    print_star();
+void func1(void){
+     int x;
+     printf("func1 x is at %p\n",&x);
+     }
+int main(void){
+    int x;
+    printf("main x is at %p\n",&x);
+    func1();
+    
     system("PAUSE"); 
     return 0;
 }
